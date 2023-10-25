e2aa64e405e9136825520d56b4a725e82d2d9573
chris-ortiz <christian.ortiz@adesso.de>
2023-10-26 01:08:48 +0200

    refactoring

--- main.go
+++ main.go
@@ -8,31 +8,52 @@ import (
 	"math/rand"
 )
 
-const scale = 5
-const width = 640
-const height = 480
-const columns = width / scale
-const rows = height / scale
-
 type Game struct {
-	spielfeld [columns][rows]bool
-}
-
-func (g *Game) Update() error {
-	//g.initializeSpielfeld()
-	return nil
+	spielfeld        [][]bool
+	scale            uint8
+	width            uint16
+	height           uint16
+	columns          uint16
+	rows             uint16
+	updatesPerSecond uint8
 }
 
 func New() *Game {
 	g := &Game{}
-	g.initializeSpielfeld()
+	g.updatesPerSecond = 10
+	g.scale = 5
+	g.width = 640
+	g.height = 480
+	g.columns = g.width / uint16(g.scale)
+	g.rows = g.height / uint16(g.scale)
+	g.spielfeld = make([][]bool, g.columns)
+	for i := range g.spielfeld {
+		g.spielfeld[i] = make([]bool, g.rows)
+	}
+
+	g.randomlyFillSpielfeld()
 
 	return g
 }
 
-func (g *Game) initializeSpielfeld() {
-	for i := 0; i < columns; i++ {
-		for j := 0; j < rows; j++ {
+func (g *Game) Start() {
+	ebiten.SetWindowSize(int(g.width), int(g.height))
+	ebiten.SetWindowTitle("Game Of Life")
+	ebiten.SetTPS(int(g.updatesPerSecond))
+
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (g *Game) Update() error {
+	g.randomlyFillSpielfeld()
+	return nil
+}
+
+func (g *Game) randomlyFillSpielfeld() {
+	for i := uint16(0); i < g.columns; i++ {
+		for j := uint16(0); j < g.rows; j++ {
 			g.spielfeld[i][j] = rand.Int()%2 == 0
 		}
 	}
@@ -41,10 +62,10 @@ func (g *Game) initializeSpielfeld() {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
 
-	for x := 0; x < width; x += scale {
-		for y := 0; y < height; y += scale {
-			if g.spielfeld[x/scale][y/scale] {
-				vector.DrawFilledRect(screen, float32(x), float32(y), scale, scale,
+	for x := uint16(0); x < g.width; x += uint16(g.scale) {
+		for y := uint16(0); y < g.height; y += uint16(g.scale) {
+			if g.spielfeld[x/uint16(g.scale)][y/uint16(g.scale)] {
+				vector.DrawFilledRect(screen, float32(x), float32(y), float32(g.scale), float32(g.scale),
 					color.Black, false)
 			}
 		}
@@ -52,18 +73,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return width, height
+	return int(g.width), int(g.height)
 }
 
 func main() {
-	ebiten.SetWindowSize(width, height)
-	ebiten.SetWindowTitle("Game Of Life")
-	ebiten.SetTPS(1)
-
 	game := New()
-
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
-
+	game.Start()
 }
